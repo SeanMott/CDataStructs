26a80e1f9104365f44bb82a4b163ac0f29758420
SeanMott <rafal.mott1@gmail.com>
2021-05-14 11:42:21 -0500

    Changed to single header.

--- List.h
+++ List.h
@@ -11,7 +11,16 @@ Author: Ianertson || Dynamic list in C
 
 #include <stdint.h>
 #include <stdbool.h>
-#include <CDataStructs\Logger.h>
+
+#ifndef C_DATA_STRUCTS_LIST_LOGGER
+#define C_DATA_STRUCTS_LIST_LOGGER
+
+#include <stdio.h>
+#define CDataStructs_List_LogWarning(type, message) printf("List Warning: %s || %s\n", type, message)
+#define CDataStructs_List_LogError(type, message) printf("List Error: %s || %s\n", type, message)
+#define CDataStructs_List_LogMessage(message) printf("%s\n", message)
+
+#endif
 
 typedef struct
 {
@@ -22,11 +31,36 @@ typedef struct
 } CDataStructs_List;
 
 //creates a List
+CDataStructs_List* CDataStructs_List_Create(size_t itemSize);
+//destroys a List
+void CDataStructs_List_Destroy(CDataStructs_List* list, void(*freeMethod)(void* item));
+//appends a item to the List || stops duplicates and NULL items
+void CDataStructs_List_Append(CDataStructs_List* list, void* item);
+//appends a item to the List || allows a duplicate and stops Null items
+void CDataStructs_List_AppendDuplicate(CDataStructs_List* list, void* item);
+//appends a item to the List || stops duplicates and allows NULL items
+void CDataStructs_List_AppendNull(CDataStructs_List* list, void* item);
+//appends a item to the List || allows duplicates and NULL items
+void CDataStructs_List_AppendNullDuplicate(CDataStructs_List* list, void* item);
+//removes a item from the List
+void CDataStructs_List_RemoveItem(CDataStructs_List* list, void* item, void(*freeMethod)(void*));
+//removes a item by position from the List
+void CDataStructs_List_RemovePos(CDataStructs_List* list, uint32_t pos, void(*freeMethod)(void*));
+//finds if a item is in the List
+bool CDataStructs_List_IsItem(CDataStructs_List* list, void* item, bool checkValidPrams);
+//gets the position of the item in the List || -1 will be returned if no item is their.
+size_t CDataStructs_List_ItemPosition(CDataStructs_List* list, void* item, bool checkValidPrams);
+//prints the contents of the List
+void CDataStructs_List_Print(CDataStructs_List* list);
+
+#ifdef CDATASTRUCTS_LIST_DEFINITIONS
+
+//creates a List
 CDataStructs_List* CDataStructs_List_Create(size_t itemSize)
 {
     if (itemSize <= 0)
     {
-        CDataStructs_LogError("List Creation", "Item Size must be greater then 0!");
+        CDataStructs_List_LogError("List Creation", "Item Size must be greater then 0!");
         return NULL;
     }
 
@@ -43,7 +77,7 @@ void CDataStructs_List_Destroy(CDataStructs_List* list, void(*freeMethod)(void* 
 {
     if (!list)
     {
-        CDataStructs_LogError("List Destruction", "List is NULL can not be destroyed!");
+        CDataStructs_List_LogError("List Destruction", "List is NULL can not be destroyed!");
         return;
     }
 
@@ -67,13 +101,13 @@ void CDataStructs_List_Append(CDataStructs_List* list, void* item)
 {
     if (!list)
     {
-        CDataStructs_LogError("List Append", "List is NULL can not append data!");
+        CDataStructs_List_LogError("List Append", "List is NULL can not append data!");
         return;
     }
 
     if (!item)
     {
-        CDataStructs_LogError("List Append", "Item is NULL can not append to List!");
+        CDataStructs_List_LogError("List Append", "Item is NULL can not append to List!");
         return;
     }
 
@@ -94,7 +128,7 @@ void CDataStructs_List_Append(CDataStructs_List* list, void* item)
             list->items[list->length - 1] = item;
         }
         else
-            CDataStructs_LogWarning("List Append Duplicate", "The List already contains that item, if you wish to add the same item uses \"AppendDuplicate\".");
+            CDataStructs_List_LogWarning("List Append Duplicate", "The List already contains that item, if you wish to add the same item uses \"AppendDuplicate\".");
     }
 }
 
@@ -103,13 +137,13 @@ void CDataStructs_List_AppendDuplicate(CDataStructs_List* list, void* item)
 {
     if (!list)
     {
-        CDataStructs_LogError("List Append", "List is NULL can not append data!");
+        CDataStructs_List_LogError("List Append", "List is NULL can not append data!");
         return;
     }
 
     if (!item)
     {
-        CDataStructs_LogError("List Append", "Item is NULL can not append to List!");
+        CDataStructs_List_LogError("List Append", "Item is NULL can not append to List!");
         return;
     }
 
@@ -134,7 +168,7 @@ void CDataStructs_List_AppendNull(CDataStructs_List* list, void* item)
 {
     if (!list)
     {
-        CDataStructs_LogError("List Append", "List is NULL can not append data!");
+        CDataStructs_List_LogError("List Append", "List is NULL can not append data!");
         return;
     }
 
@@ -155,7 +189,7 @@ void CDataStructs_List_AppendNull(CDataStructs_List* list, void* item)
             list->items[list->length - 1] = item;
         }
         else
-            CDataStructs_LogWarning("List Append Duplicate", "The List already contains that item, if you wish to add the same item uses \"AppendDuplicate\".");
+            CDataStructs_List_LogWarning("List Append Duplicate", "The List already contains that item, if you wish to add the same item uses \"AppendDuplicate\".");
     }
 }
 
@@ -164,7 +198,7 @@ void CDataStructs_List_AppendNullDuplicate(CDataStructs_List* list, void* item)
 {
     if (!list)
     {
-        CDataStructs_LogError("List Append", "List is NULL can not append data!");
+        CDataStructs_List_LogError("List Append", "List is NULL can not append data!");
         return;
     }
 
@@ -189,26 +223,26 @@ void CDataStructs_List_RemoveItem(CDataStructs_List* list, void* item, void(*fre
 {
     if (!list)
     {
-        CDataStructs_LogError("List Remove", "List is NULL can not remove items!");
+        CDataStructs_List_LogError("List Remove", "List is NULL can not remove items!");
         return;
     }
 
     if (!item)
     {
-        CDataStructs_LogError("List Remove", "Item is NULL can not remove items!");
+        CDataStructs_List_LogError("List Remove", "Item is NULL can not remove items!");
         return;
     }
 
     if (list->length <= 0 || !list->items)
     {
-        CDataStructs_LogWarning("List Remove", "The List has no items, add some with \"Append\" or \"AppendDuplicate\" first.");
+        CDataStructs_List_LogWarning("List Remove", "The List has no items, add some with \"Append\" or \"AppendDuplicate\" first.");
         return;
     }
 
     int32_t pos = CDataStructs_List_ItemPosition(list, item, false);
     if (pos == -1)
     {
-        CDataStructs_LogWarning("List Remove", "The List does not contain the give item, check the pointer or add it with \"Append\" or \"AppendDuplicate\" first.");
+        CDataStructs_List_LogWarning("List Remove", "The List does not contain the give item, check the pointer or add it with \"Append\" or \"AppendDuplicate\" first.");
         return;
     }
     CDataStructs_List_ShiftItemsLeft(list, pos);
@@ -225,19 +259,19 @@ void CDataStructs_List_RemovePos(CDataStructs_List* list, uint32_t pos, void(*fr
 {
     if (!list)
     {
-        CDataStructs_LogError("List Remove", "List is NULL can not remove items!");
+        CDataStructs_List_LogError("List Remove", "List is NULL can not remove items!");
         return;
     }
 
     if (list->length <= 0 || !list->items)
     {
-        CDataStructs_LogWarning("List Remove", "The List has no items, add some with \"Append\" or \"AppendDuplicate\" first.");
+        CDataStructs_List_LogWarning("List Remove", "The List has no items, add some with \"Append\" or \"AppendDuplicate\" first.");
         return;
     }
 
     if (pos == -1 || pos >= list->length)
     {
-        CDataStructs_LogData("CDataStructs Error: List Remove || The List does not have a item at %i, append one with \"Append\" or \"AppendDuplicate\" first!\n", pos);
+        CDataStructs_List_LogData("CDataStructs Error: List Remove || The List does not have a item at %i, append one with \"Append\" or \"AppendDuplicate\" first!\n", pos);
         return;
     }
     if (freeMethod)
@@ -248,7 +282,7 @@ void CDataStructs_List_RemovePos(CDataStructs_List* list, uint32_t pos, void(*fr
 
     list->items = realloc(list->items, list->itemSize * list->length);
 
-    CDataStructs_LogMessage("CDataStructs_List_RemovePos is currently not supported. Check Github for updates.");
+    CDataStructs_List_LogMessage("CDataStructs_List_RemovePos is currently not supported. Check Github for updates.");
 }
 
 //finds if a item is in the List
@@ -258,13 +292,13 @@ bool CDataStructs_List_IsItem(CDataStructs_List* list, void* item, bool checkVal
     {
         if (!list)
         {
-            CDataStructs_LogError("List", "List is NULL can not check for the item, returning false!");
+            CDataStructs_List_LogError("List", "List is NULL can not check for the item, returning false!");
             return false;
         }
 
         if (!item)
         {
-            CDataStructs_LogError("List", "Item is NULL can not check for the item, returning false!");
+            CDataStructs_List_LogError("List", "Item is NULL can not check for the item, returning false!");
             return false;
         }
     }
@@ -285,13 +319,13 @@ size_t CDataStructs_List_ItemPosition(CDataStructs_List* list, void* item, bool 
     {
         if (!list)
         {
-            CDataStructs_LogError("List", "List is NULL can not check the position of a item, returning -1!");
+            CDataStructs_List_LogError("List", "List is NULL can not check the position of a item, returning -1!");
             return -1;
         }
 
         if (!item)
         {
-            CDataStructs_LogError("List", "Item is NULL can not check the position of a item, returning -1!");
+            CDataStructs_List_LogError("List", "Item is NULL can not check the position of a item, returning -1!");
             return -1;
         }
     }
@@ -310,11 +344,11 @@ void CDataStructs_List_Print(CDataStructs_List* list)
 {
     if (!list)
     {
-        CDataStructs_LogError("List", "List is Null can not print it's contents!");
+        CDataStructs_List_LogError("List", "List is Null can not print it's contents!");
         return;
     }
 
-    CDataStructs_LogData("CDataStructs_List: Item Count: %i, Item Memory Size: %i, List Total Size: %i\n", list->length,
+    CDataStructs_List_LogData("CDataStructs_List: Item Count: %i, Item Memory Size: %i, List Total Size: %i\n", list->length,
         list->itemSize, list->itemSize * list->length);
 }
 
@@ -327,5 +361,7 @@ inline void CDataStructs_List_ShiftItemsLeft(CDataStructs_List* list, int32_t in
     for (index; index < list->length - 1; index++)
         list->items[index] = list->items[index + 1];
 }
+
+#endif
 
 #endif
--- README.md
+++ README.md
@@ -9,4 +9,6 @@ A collection of differnt data structures for C. Sources for info on making the s
 - [Ianertson || Dynamic list in C](https://www.youtube.com/watch?v=QKZXQc8EBDk&t=1004s)
 
 # Building
-Builds with Premake5, script is provided in the root directory.
+The data structures are single header files. Pick any C or C++ file and before including write "#define CDATASTRUCTS_ -Data struct - _DEFINITIONS. This will create the definitions. Below is a list of the defines <br>
+<br>
+- List || CDATASTRUCTS_LIST_DEFINITIONS
